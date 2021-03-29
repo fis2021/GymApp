5bfff32d42a810742f87b66f76fa1c9046ad960c
Rares Matei <rares.matei171@gmail.com>
2021-03-29 18:47:57 +0300

    Added models: Admin, Customer, GymManager Sprint 1 issue GA-5

---
+++ src/proiect/fis/gym/aplication/model/Admin.java
@@ -0,0 +1,95 @@
+package proiect.fis.gym.aplication.model;
+
+import java.util.Objects;
+
+public class Admin {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    //cod pt register ca admin
+    private static String companyCode;
+
+    public Admin(String firstName, String lastName, String username, String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Admin admin = (Admin) o;
+        return Objects.equals(firstName, admin.firstName) &&
+                Objects.equals(lastName, admin.lastName) &&
+                Objects.equals(username, admin.username) &&
+                Objects.equals(password, admin.password);
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, username, password);
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static void setCompanyCode(String companyCode) {
+        Admin.companyCode = companyCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static String getCompanyCode() {
+        return companyCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
