111d8444b8ec7db8dbca6d006addedea5c0049c2
Geo <georgeX_D@yahoo.com>
2021-03-30 11:54:19 +0300

    Am adaugat niste passwordField si textField.

--- src/proiect/fis/gym/aplication/controllers/RegisterAdminController.java
+++ src/proiect/fis/gym/aplication/controllers/RegisterAdminController.java
@@ -3,10 +3,18 @@ package proiect.fis.gym.aplication.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class RegisterAdminController extends RegisterController {
     @FXML
     private ChoiceBox<String> selectRoleChoiceBox;
+
+    @FXML
+    private TextField firstNameField,lastNameField,emailField,usernameField;
+
+    @FXML
+    private PasswordField passwordField;
 
     @FXML
     public void initialize(){
