0b1fe7797f2ce15506374e3b97992e8ebbc1e964
Rares Matei <rares.matei171@gmail.com>
2021-04-01 19:29:45 +0300

    Register for Admin is ready with bugs fixed Sprint1 GA 5 GA25

--- src/proiect/fis/gym/aplication/controllers/RegisterController.java
+++ src/proiect/fis/gym/aplication/controllers/RegisterController.java
@@ -3,22 +3,28 @@ package proiect.fis.gym.aplication.controllers;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Control;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class RegisterController {
 
-    public void selectRoleChoiceBoxAction(ChoiceBox<String> choiceBox){
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static final String VALID_PASSWORD_REGEX =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=_])(?=\\S+$).{8,20}$";
+
+    public void selectRoleChoiceBoxAction(ChoiceBox<String> choiceBox) {
         choiceBox.getSelectionModel().selectedItemProperty().addListener
                 ((v, oldValue, newValue) ->
                         {
                             try {
                                 Stage stage = (Stage) choiceBox.getScene().getWindow();
-                                String loadedFXML = "register" + newValue.replace(" ", "") + ".fxml" ;
+                                String loadedFXML = "register" + newValue.replace(" ", "") + ".fxml";
                                 String path = "../fxml/" + loadedFXML;
                                 Parent viewRegisterRoot = FXMLLoader.load(getClass().getResource(path));
                                 Scene scene = new Scene(viewRegisterRoot, 700, 500);
@@ -28,6 +34,49 @@ public abstract class RegisterController {
                             }
                         }
                 );
+    }
+
+    public void passwordMatcher(TextField passwordField, TextField confirmPasswordField, Label warningLabel){
+        confirmPasswordField.textProperty().addListener((observable, oldValue, newValue) ->
+        {
+            //System.out.println("textfield changed from " + oldValue + " to " + newValue);
+
+            if (!passwordField.getText().equals(confirmPasswordField.getText()) ){
+                warningLabel.setVisible(true);
+            }
+            else{
+                warningLabel.setVisible(false);
+            }
+        });
+
+    }
+
+    public void checkEmail(TextField emailField, Label warningLabel) {
+
+        emailField.textProperty().addListener((observable, oldValue, newValue) ->
+        {
+            Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(newValue);
+            if (!matcher.find()){
+                warningLabel.setVisible(true);
+                //warningLabel.setText(=);
+            }
+            else{
+                warningLabel.setVisible(false);
+            }
+        });
+    }
+
+    public void passwordNotMatchingRegex(PasswordField passwordField, Label warningLabel){
+        passwordField.textProperty().addListener((observable, oldValue, newValue) ->
+        {
+            //Matcher matcher = VALID_PASSWORD_REGEX.matches(newValue);
+            if (!newValue.matches(VALID_PASSWORD_REGEX)){
+                warningLabel.setVisible(true);
+            }
+            else{
+                warningLabel.setVisible(false);
+            }
+        });
     }
 
     public void handleBackToLoginButtonLogic(Control control){
