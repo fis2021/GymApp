384fa55ecba83fc97eb01cd8f4c8af716a0d5adb
Geo <georgeX_D@yahoo.com>
2021-04-14 17:24:42 +0300

    Added a subscription button for every gym, Sprint 2, story GA-12, subtask GA-47. Added a gym list, Sprint 2, story GA-10, subtask GA-43.

--- src/proiect/fis/gym/aplication/Main.java
+++ src/proiect/fis/gym/aplication/Main.java
@@ -28,9 +28,9 @@ public class Main extends Application {
         CustomerService.initDatabase();
         GymManagerService.initDatabase();
         LoginService.initDatabase();
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/CustomerAfterLoginPage.fxml"));
         primaryStage.setTitle("Gym Aplication");
-        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
     }
 
