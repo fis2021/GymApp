7b1a35623951172ce35307a8a93f6c787345e610
rareshM <rares.matei171@gmail.com>
2021-05-10 11:58:06 +0300

    modified openNewScene method in CommonFunctionality

--- src/main/java/proiect/fis/gym/aplication/controllers/CommonFunctionality.java
+++ src/main/java/proiect/fis/gym/aplication/controllers/CommonFunctionality.java
@@ -1,5 +1,6 @@
 package proiect.fis.gym.aplication.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.TextArea;
@@ -10,12 +11,20 @@ import javafx.scene.control.Control;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import proiect.fis.gym.aplication.exceptions.FieldsAreNotEmptyException;
+import proiect.fis.gym.aplication.exceptions.IncorectCVCException;
+import proiect.fis.gym.aplication.exceptions.IncorectCardNumberException;
+import proiect.fis.gym.aplication.exceptions.incorectCardDetailsException;
+import proiect.fis.gym.aplication.model.Bank;
+import proiect.fis.gym.aplication.model.GymManager;
+import proiect.fis.gym.aplication.services.BankService;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 
 public class CommonFunctionality {
-    
+
     //checks if the text fields from a pane are empty
     public static boolean checkTextFieldsInAPaneAreNotEmpty(Pane pane) {
         for(Node node: pane.getChildren()){
@@ -40,17 +49,42 @@ public class CommonFunctionality {
         return true;
     }
 
-    public void openNewScene(String fxmlLoaded, Control control,int a, int b){
+    public void openNewScene(String fxmlLoaded, Control control, int width, int height){
         try {
             Stage stage = (Stage) control.getScene().getWindow();
             String path = "fxml/" + fxmlLoaded;
             //System.out.println(path);
             Parent viewRegisterRoot = FXMLLoader.load(getClass().getClassLoader().getResource(path));
-            Scene scene = new Scene(viewRegisterRoot, a, b);
+
+            Scene scene = new Scene(viewRegisterRoot, width, height);
             stage.setScene(scene);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static void CVCException(String CVC) throws IncorectCVCException {
+        if(CVC.length()!=3){
+            throw new IncorectCVCException();
+        }
+    }
+
+    public static void cardNumberException(String cardN) throws IncorectCardNumberException {
+        String regex= "^\\d{16}$";
+        Pattern pat = Pattern.compile(regex);
+        if( !(pat.matcher(cardN).matches()) )
+            throw new IncorectCardNumberException();
+    }
+
+    public static void cardDetailsException(String cardOwnerName,String expM,String expY,String cardN,String CVC) throws incorectCardDetailsException {
+        int ok=0;
+        for(Bank bank : BankService.getBankRepository().find()){
+            if(cardOwnerName.equals(bank.getNumeDetinator()) && expM.equals(bank.getLuna()) && expY.equals(bank.getAnu()) && cardN.equals(bank.getNumarCard()) && CVC.equals(bank.getCVC())){
+                ok=1;
+                break;
+            }
+        }
+        if(ok==0)
+            throw new incorectCardDetailsException();
+    }
 }
